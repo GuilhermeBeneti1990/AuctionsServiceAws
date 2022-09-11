02098013f3c0574408586ffcdeccbd74f1aea24e
GuilhermeBeneti1990 <guilhermebeneti@icloud.com>
2022-09-11 10:47:06 -0300

    s3 bucket image auction upload implemented

---
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -0,0 +1,42 @@
+import { getAuctionById } from './getAuction';
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
+import middy from '@middy/core';
+import cors from '@middy/http-cors';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+import validator from '@middy/validator';
+import uploadAuctionPictureSchema from '../schemas/uploadAuctionPictureSchema';
+
+export async function uploadAuctionPicture(event) {
+    const { id } = event.pathParameters;
+    const { email } = event.requestContext.authorizer;
+    const auction = await getAuctionById(id);
+
+    if (auction.seller !== email) {
+        throw new createError.Forbidden('You are not the seller of this auction!');
+    }
+
+    const base64 = event.body.replace(/ˆdata:image\/\w+;base64,/, '');
+    const buffer = Buffer.from(base64, 'base64');
+
+    let updatedAuction;
+
+    try {
+        const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+        updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+    } catch(error) {
+        console.log(error);
+        throw new createError.InternalServerError(error);
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(updatedAuction)
+    }
+}
+
+export const handler = middy(uploadAuctionPicture)
+    .use(httpErrorHandler())
+    .use(validator({ inputSchema: uploadAuctionPictureSchema }))
+    .use(cors());
